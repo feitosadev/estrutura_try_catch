fd2fab86402eb360658c847ca86e9681c46de955
Jose Carlos Feitosa <92383795+feitosadev@users.noreply.github.com>
2025-06-18 21:35:07 -0300

    Add files via upload

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,35 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Scanner entrada = new Scanner(System.in);
+		metodo1();
 		
-		String[] vetor = entrada.nextLine().split(" ");
-		int posicao = entrada.nextInt();
-		System.out.println(vetor[posicao]);
+	}
+	
+	public static void metodo1() {
+		System.out.println("Metodo 1 foi chamado.");
+		metodo2();
+		System.out.println("Metodo 1 foi encerrado.");
+	}
+	
+	public static void metodo2() {
+		System.out.println("Metodo 2 foi chamado");
+		Scanner entrada = new Scanner(System.in);
+		try {
+			String[] vetor = entrada.nextLine().split(" ");
+			int posicao = entrada.nextInt();
+			System.out.println(vetor[posicao]);
+		} catch (InputMismatchException e) {
+			System.out.println("Você não digitou um número.");
+		}catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("Número fora do tamanho do vetor.");
+		}
+		
+		System.out.println("Metodo 2 foi encerrado.");
 	}
 }
